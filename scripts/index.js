d010ba034d0b2ac30d7c5cc463528c07ed365d34
Uliana Myasischeva <ud.myasischeva@gmail.com>
2024-02-02 01:09:41 +0300

    first commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document.getElementById("card-template").content;
+
 // @todo: DOM узлы
+
+const cardContainer = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
 
+function createCard(cardData, deleteCard) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  cardImage.src = cardData.link;
+  cardTitle.textContent = cardData.name;
+  cardImage.alt = `На фото ${cardData.name}`;
+
+  deleteButton.addEventListener("click", () => deleteCard(deleteButton));
+
+  return cardElement;
+}
 // @todo: Функция удаления карточки
 
+function deleteCard(button) {
+  button.closest(".card").remove();
+}
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach((cardElement) => {
+  cardContainer.append(createCard(cardElement, deleteCard));
+});
